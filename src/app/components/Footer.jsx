e7b53d72e579bd28e1538e753036036f21432256
Don aldrich Tamayo <amazingmardon@gmail.com>
2018-11-05 15:48:54 -0500

    added more

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Footer extends React.Component{
+    render() {
+        return (
+            <footer>
+                <small>
+                    You can find the code repo on <a href="">Github</a>. 
+                </small>
+                <p>Built with <span>&hearts;</span> by <a href="http://dontamayo.com">More on DonTamayo.com</a></p>
+            </footer>
+        )
+    }
+}
+
+export default Footer
